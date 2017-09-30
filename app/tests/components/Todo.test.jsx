d700db9015019868f8d0326831fbe2a2347e533e
MMG <ommv@yahoo.com>
2017-10-01 00:38:31 +1000

    Create AddTodo component

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var recatDOM = require('react-dom');
+var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var expect = require('expect');
 var $ = require('jquery');
