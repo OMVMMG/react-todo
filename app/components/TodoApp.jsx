881e016b7fd82181f97f64b593adef18725b0b0b
MMG <ommv@yahoo.com>
2018-09-26 23:26:09 +1000

    Refactor app to work with local storage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -8,52 +8,52 @@ import TodoList from 'TodoList'
 import AddTodo from 'AddTodo';
 //var TodoSearch = require('TodoSearch');
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
+// var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      showCompleted: false,
-      searchText: '',
-      todos: TodoAPI.getTodos()
-      //[
-      //   {
-      //     id: uuid(),
-      //     text: 'Walk the dog',
-      //     completed: false
-      //   }, {
-      //     id: uuid(),
-      //     text: 'Clean the yard',
-      //     completed: true
-      //   }, {
-      //     id: uuid(),
-      //     text: 'Feed the cat',
-      //     completed: true
-      //   }, {
-      //     id: uuid(),
-      //     text: 'Wash the car',
-      //     completed: false
-      //   }
-      // ]
-    };
-  },
-  componentDidUpdate: function () {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleAddTodo: function (text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    });
-  },
+  // getInitialState: function () {
+  //   return {
+  //     showCompleted: false,
+  //     searchText: '',
+  //     todos: TodoAPI.getTodos()
+  //     //[
+  //     //   {
+  //     //     id: uuid(),
+  //     //     text: 'Walk the dog',
+  //     //     completed: false
+  //     //   }, {
+  //     //     id: uuid(),
+  //     //     text: 'Clean the yard',
+  //     //     completed: true
+  //     //   }, {
+  //     //     id: uuid(),
+  //     //     text: 'Feed the cat',
+  //     //     completed: true
+  //     //   }, {
+  //     //     id: uuid(),
+  //     //     text: 'Wash the car',
+  //     //     completed: false
+  //     //   }
+  //     // ]
+  //   };
+  // },
+  // componentDidUpdate: function () {
+  //   TodoAPI.setTodos(this.state.todos);
+  // },
+  // handleAddTodo: function (text) {
+  //   this.setState({
+  //     todos: [
+  //       ...this.state.todos,
+  //       {
+  //         id: uuid(),
+  //         text: text,
+  //         completed: false,
+  //         createdAt: moment().unix(),
+  //         completedAt: undefined
+  //       }
+  //     ]
+  //   });
+  // },
   // handleToggle: function (id) {
   //   var updatedTodos =this.state.todos.map((todo) => {
   //     if (todo.id === id) {
@@ -65,15 +65,15 @@ var TodoApp = React.createClass({
   //   });
   //   this.setState({todos: updatedTodos});
   // },
-  handleSearch: function (showCompleted, searchText) {
-      this.setState({
-        showCompleted: showCompleted,
-        searchText: searchText.toLowerCase()
-      });
-    },
+  // handleSearch: function (showCompleted, searchText) {
+  //     this.setState({
+  //       showCompleted: showCompleted,
+  //       searchText: searchText.toLowerCase()
+  //     });
+  //   },
     render: function () {
-      var {todos, showCompleted, searchText} = this.state;
-      var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+      // var {todos, showCompleted, searchText} = this.state;
+      // var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
       return (
         <div>
@@ -82,9 +82,9 @@ var TodoApp = React.createClass({
           <div className="row">
             <div className="column small-centered small-11 medium-6 large-5">
               <div className="container">
-                <TodoSearch onSearch={this.handleSearch}/>
+                <TodoSearch/>
                 <TodoList/>
-                <AddTodo onAddTodo={this.handleAddTodo}/>
+                <AddTodo/>
               </div>
             </div>
           </div>
