9d678a1cde72313d9f0f31ebc9e99355dfdc6fbc
MMG <ommv@yahoo.com>
2019-07-15 23:17:47 +1000

    Add react-router middleware and redirect on auth

--- app/app.jsx
+++ app/app.jsx
@@ -1,47 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
-//var TodoApp = require('TodoApp');
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
-import Login from 'Login';
-import TodoApp from 'TodoApp';
+import firebase from 'app/firebase/';
+import router from 'app/router/';
 
-//import './../playground/firebase/index';
-
-// store.subscribe(() => {
-//   var state = store.getState();
-//   console.log('New state', state);
-//   TodoAPI.setTodos(state.todos);
-// });
-
-// var initialTodos = TodoAPI.getTodos();
-// store.dispatch(actions.addTodos(initialTodos));
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    hashHistory.push('/todos');
+  } else {
+    hashHistory.push('/');
+  }
+});
 
 store.dispatch(actions.startAddTodos());
 
-// store.dispatch(actions.addTodo('Clean the yard'));
-// store.dispatch(actions.setSearchText('yard'));
-// store.dispatch(actions.toggleShowCompleted());
-
-//Load foundation
-//require('style!css!foundation-sites/dist/foundation.min.css')
+// Load foundation
 $(document).foundation();
 
-//App css
+// App css
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-    <Provider store={store}>
-      <Router history={hashHistory}>
-        <Route path="/">
-          <Route path="todos" component={TodoApp} />
-          <IndexRoute component={Login} />
-        </Route>
-      </Router>
-    </Provider>,
-    document.getElementById('app')
+  <Provider store={store}>
+    {router}
+  </Provider>,
+  document.getElementById('app')
 );
--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 try {
   var config = {
-     apiKey: process.env.API_KEY,
-     authDomain: process.env.AUTH_DOMAIN,
-     databaseURL: process.env.DATABASE_URL,
-     storageBucket: process.env.STORAGE_BUCKET,
-   };
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+  };
 
-   firebase.initializeApp(config);
+  firebase.initializeApp(config);
 } catch (e) {
 
 }
